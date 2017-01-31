eda44c76d7a939ad7b9b60bd026bcdbdaa4ee778
Böhme, Christian <christian.boehme@accenture.com>
2017-01-31 13:31:49 +0100

    - Postgres Dependencies & Simple Connection

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -5,9 +5,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.*;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.*;
+import org.apache.commons.dbcp2.*;
+
+
+
 @Controller
 @SpringBootApplication
 public class DemoApplication {
+
+
+    private BasicDataSource connectionPool;
+
 
     @RequestMapping("/")
     @ResponseBody
@@ -18,4 +29,29 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
+
+    public DemoApplication() throws URISyntaxException, SQLException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
+        connectionPool = new BasicDataSource();
+
+        if (dbUri.getUserInfo() != null) {
+            connectionPool.setUsername(dbUri.getUserInfo().split(":")[0]);
+            connectionPool.setPassword(dbUri.getUserInfo().split(":")[1]);
+        }
+        connectionPool.setDriverClassName("org.postgresql.Driver");
+        connectionPool.setUrl(dbUrl);
+        connectionPool.setInitialSize(1);
+
+        Connection connection = connectionPool.getConnection();
+
+        Statement stmt = connection.createStatement();
+        stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+        stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+        ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+        while (rs.next()) {
+            System.out.println("Read from DB: " + rs.getTimestamp("tick") + "\n");
+        }
+    }
+
 }
