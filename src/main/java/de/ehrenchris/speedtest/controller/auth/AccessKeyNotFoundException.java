3acb91ee1f3c5feb4eb1a73cde92ce210a3ea92c
Böhme, Christian <christian.boehme@accenture.com>
2017-02-03 15:37:02 +0100

    - Suppress Warning

--- src/main/java/de/ehrenchris/speedtest/controller/auth/AccessKeyNotFoundException.java
+++ src/main/java/de/ehrenchris/speedtest/controller/auth/AccessKeyNotFoundException.java
@@ -10,10 +10,10 @@ import org.springframework.web.bind.annotation.ResponseStatus;
  * @author christian.boehme
  *
  */
+@SuppressWarnings("serial")
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class AccessKeyNotFoundException extends RuntimeException {
 
-	
 	public AccessKeyNotFoundException(String accessKey){
 		super("Could not find access key: " + accessKey + ".");
 	}
